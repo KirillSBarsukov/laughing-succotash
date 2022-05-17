1b23b23f8cc1ce822a6c0df674a192e11e92b424
Kirill Barsukov <kirillb@avetti.ca>
2022-05-17 20:17:49 +0300

    test

--- dist/index.esm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TestComponent = () => (React.createElement("div", { "data-testid": "test-component" },
-    React.createElement("h1", { className: "heading" }, "I'm the test component"),
-    React.createElement("h2", null, "Made with love by Harvey")));
-
-export { TestComponent };
-//# sourceMappingURL=index.esm.js.map
